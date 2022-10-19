aef58f52d0bc0d98414a1ce7f03bf2f001b2062c
RiskiSarma <riski.sarma28@gmail.com>
2022-10-19 11:50:43 +0700

    cal
    tes

---
+++ src/Project1/Calculator.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Calculator {
+    String Nama;
+
+    public Calculator(String Nama) {
+        this.Nama = Nama;
+    }
+    
+}
