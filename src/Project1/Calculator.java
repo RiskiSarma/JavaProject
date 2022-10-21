9993a75c5fd4c08c692ee09b5e4832da92e7d10c
zakyabrar <zakyabrar79@gmail.com>
2022-10-21 13:46:54 +0700

    Zaky Abrar : Luas Trapesium

--- src/Project1/Calculator.java
+++ src/Project1/Calculator.java
@@ -3,66 +3,77 @@ package Project1;
 public class Calculator {
     String nama;
 
-    Calculator(String nama){
+    Calculator(String nama) {
         this.nama = nama;
     }
 
- //rizka aulia
-    public double penjumlahan(double bil1, double bil2){
-        double hasil=bil1+bil2;
+    // rizka aulia
+    public double penjumlahan(double bil1, double bil2) {
+        double hasil = bil1 + bil2;
         return hasil;
     }
-    //Dea Syafira
-    public double perkalian(double bil1, double bil2){
-        double hasil=bil1*bil2;
+
+    // Dea Syafira
+    public double perkalian(double bil1, double bil2) {
+        double hasil = bil1 * bil2;
         return hasil;
     }
-    //Fuzla Azkia
-     public int modulus(int angka1,int angka2){
-    int hasil = angka1 % angka2;
-    return hasil;
-}
 
-// Nurjannatul Husna
-public int pembagian(int angka1, int angka2) {
-    int hasil = angka1 / angka2;
-    return hasil;
-}
-
-//Mitha Zahara
-public double pangkat(int angka1, int pangkat){
-    int hasil=1;
-    for(int i=1;i<=pangkat;i++){
-        hasil=hasil*angka1;
-    }
-    return hasil;
-}
-//Dhynanti Amanda
- public int Persentase(int bil1,int bil2){
-      int  persent;
-      persent=((bil1*100)/bil2);
-    System.out.print(" Dhynanti hasil persen nya adalah : " +persent);
-    System.out.print("%");
-    return persent;
-    }
-
-//Muhammad Amzar
-      public int KelilingPersegi(int sisi){
-            return sisi+sisi+sisi+sisi;
-        }
-        //alfi syahrin
-        public double sin(double nilai){
-            return Math.sin(nilai);
-        }
-//haiqal murthada
-        public double luasPersegiPanjang(double panjang, double lebar){
-            return panjang*lebar;
-        }
-//munawar iman
-   public int luaskubus(int sisi){
-         int hasil= 6*sisi*sisi;
+    // Fuzla Azkia
+    public int modulus(int angka1, int angka2) {
+        int hasil = angka1 % angka2;
         return hasil;
-}
     }
 
+    // Nurjannatul Husna
+    public int pembagian(int angka1, int angka2) {
+        int hasil = angka1 / angka2;
+        return hasil;
+    }
 
+    // Mitha Zahara
+    public double pangkat(int angka1, int pangkat) {
+        int hasil = 1;
+        for (int i = 1; i <= pangkat; i++) {
+            hasil = hasil * angka1;
+        }
+        return hasil;
+    }
+
+    // Dhynanti Amanda
+    public int Persentase(int bil1, int bil2) {
+        int persent;
+        persent = ((bil1 * 100) / bil2);
+        System.out.print(" Dhynanti hasil persen nya adalah : " + persent);
+        System.out.print("%");
+        return persent;
+    }
+
+    // Muhammad Amzar
+    public int KelilingPersegi(int sisi) {
+        return sisi + sisi + sisi + sisi;
+    }
+
+    // alfi syahrin
+    public double sin(double nilai) {
+        return Math.sin(nilai);
+    }
+
+    // haiqal murthada
+    public double luasPersegiPanjang(double panjang, double lebar) {
+        return panjang * lebar;
+    }
+
+    // munawar iman
+    public int luaskubus(int sisi) {
+        int hasil = 6 * sisi * sisi;
+        return hasil;
+    }
+
+    // Zaky Abrar
+    public double luastrapesium(double t, double a, double b) {
+        double l = ((a + b) * t);
+        double u = 1.5 * l;
+        return u;
+    }
+}
--- src/Project1/ClassMain.java
+++ src/Project1/ClassMain.java
@@ -3,35 +3,38 @@ package Project1;
 public class ClassMain {
     public static void main(String[] args) {
         Calculator orizkaaulia = new Calculator("Rizka Aulia");
-        System.out.println(orizkaaulia.nama+" Hasil penjumlahan : " + orizkaaulia.penjumlahan(10,10));
-    
+        System.out.println(orizkaaulia.nama + " Hasil penjumlahan : " + orizkaaulia.penjumlahan(10, 10));
+
         Calculator oDea = new Calculator("Dea Syafira");
-        System.out.println(oDea.nama+" Hasil perkalian : " + oDea.perkalian(10,10));
-        
+        System.out.println(oDea.nama + " Hasil perkalian : " + oDea.perkalian(10, 10));
+
         Calculator ofuzlaazkia = new Calculator("Fuzla azkia");
-        System.out.println(ofuzlaazkia.nama + " Hasil Modulus : " + ofuzlaazkia.modulus (20,3));
+        System.out.println(ofuzlaazkia.nama + " Hasil Modulus : " + ofuzlaazkia.modulus(20, 3));
 
         Calculator oHusna = new Calculator("Husna");
-        System.out.println(oHusna.nama + " Hasil pembagian : " + oHusna.pembagian (20,3));
-    
+        System.out.println(oHusna.nama + " Hasil pembagian : " + oHusna.pembagian(20, 3));
+
         Calculator omitha = new Calculator("mitha");
         System.out.println(omitha.nama + " Hasil pangkat : " + omitha.pangkat(2, 4));
-        
+
         Calculator oDhynanti = new Calculator("Dhynanti");
-         oDhynanti.Persentase(2, 20);
+        oDhynanti.Persentase(2, 20);
 
-         Calculator oAmzar = new Calculator("Muhammad amzar");
-         System.out.println(oAmzar.nama + " Hasil Keliling persegi : " + oAmzar.KelilingPersegi(10));
+        Calculator oAmzar = new Calculator("Muhammad amzar");
+        System.out.println(oAmzar.nama + " Hasil Keliling persegi : " + oAmzar.KelilingPersegi(10));
 
-         Calculator oAlfi = new Calculator("Alfi Syahrin");
-         System.out.println(oAlfi.nama + " sin : " + oAlfi.sin(5));
+        Calculator oAlfi = new Calculator("Alfi Syahrin");
+        System.out.println(oAlfi.nama + " sin : " + oAlfi.sin(5));
 
-         Calculator oHaiqal = new Calculator("haiqal murthada");
-         System.out.println(oHaiqal.nama + " luas persegi panjang : " + oHaiqal.luasPersegiPanjang(5,5));
+        Calculator oHaiqal = new Calculator("haiqal murthada");
+        System.out.println(oHaiqal.nama + " luas persegi panjang : " + oHaiqal.luasPersegiPanjang(5, 5));
 
-         Calculator oMunawarIman = new Calculator("munawar iman");
-         System.out.println(oMunawarIman.nama + " hasil luas kubus : " + oMunawarIman.luaskubus(6));
- 
+        Calculator oMunawarIman = new Calculator("munawar iman");
+        System.out.println(oMunawarIman.nama + " hasil luas kubus : " + oMunawarIman.luaskubus(6));
+
+        Calculator oZakyAbrar = new Calculator("Zaky Abrar");
+        System.out.println(oZakyAbrar.nama + " hasil luas trapesium : " + oZakyAbrar.luastrapesium(6, 7, 4));
+
     }
 
-    }
+}
